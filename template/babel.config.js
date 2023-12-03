90a6d0b9ab04f2cfe66e265cd9f507bb4a2acb64
INJAS <webinjas@gmail.com>
2023-12-03 19:23:10 +0530

    configured alias

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          '@src': './src',
+          '@screens': "./src/screens"
+        },
+      },
+    ],
+  ]
 };
--- template/src/AppNavigator.tsx
+++ template/src/AppNavigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {Home} from './Home';
+import {Home} from '@screens';
 
 const Stack = createStackNavigator();
 
