9e72359eb8ddc00be0191e055339037cff5bab7e
injas427 <webinjas@gmail.com>
2024-01-27 15:37:11 +0530

    added eslint import rule (#3)

--- template/src/AppNavigator.tsx
+++ template/src/AppNavigator.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
+import {SCREEN_NAMES} from '@constants';
 import {Home} from '@screens';
 
 const Stack = createStackNavigator();
@@ -8,7 +10,7 @@ const Stack = createStackNavigator();
 export const AppNavigator = () => (
   <NavigationContainer>
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name={SCREEN_NAMES.HOME} component={Home} />
     </Stack.Navigator>
   </NavigationContainer>
 );
